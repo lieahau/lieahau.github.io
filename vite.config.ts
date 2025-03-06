f605748dd2967995b3cebba20328764ade48e788
Budiman Putra Jaya <budimanputrajaya2@gmail.com>
2025-03-06 10:52:48 +0700

    adjust deployment config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/lieahau.github.io/",
   plugins: [react()],
-})
+});
